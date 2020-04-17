1c26208e98e53d07df807c0600805892998d2fe0
Michael Sydney Moore <michaelsydneymoore@hotmail.com>
2020-04-17 21:09:31 +0100

    Added some simple styling

--- src/App.js
+++ src/App.js
@@ -13,40 +13,33 @@ function App() {
       const repoName = response.data.map((item) => item.name);
       const repoDescription = response.data.map((item) => item.description);
       const githubLink = response.data.map((item) => item.html_url);
+      const updatedAt = response.data.map((item) => item.updated_at);
+      console.log(updatedAt);
       const displayArray = repoName.reduce(
         (acc, i, ind) =>
           acc.push(i, repoDescription[ind], githubLink[ind]) && acc,
         []
       );
       setDescrip([...displayArray]);
-      console.log(displayArray);
     }
     getData();
   }, []);
-  console.log(descrip.map((item) => item));
 
   // Wrapped in useEffect to stop it re rendering
 
   return (
-    <div style={{ border: "1px solid red" }} className="flex">
-      {/* <div>
-        {name.map((item) => (
-          <div>{item}</div>
-        ))}
-      </div> */}
-      <div>
-        {descrip.map((item, index) => {
-          if (item === null) {
-            return <div className="description">{"no description"}</div>;
-          }
-          if (index % 3 === 0) {
-            return <div className="title">{item}</div>;
-          }
-          if ((index + 1) % 3 === 0) {
-            return <a href={item}>{"Github Link"}</a>;
-          } else return <div className="description">{item}</div>;
-        })}
-      </div>
+    <div className="flex">
+      {descrip.map((item, index) => {
+        if (item === null) {
+          return <div className="description">{"no description"}</div>;
+        }
+        if (index % 3 === 0) {
+          return <div className="title">{item}</div>;
+        }
+        if ((index + 1) % 3 === 0) {
+          return <a href={item}>{"Github Link"}</a>;
+        } else return <div className="description">{item}</div>;
+      })}
     </div>
   );
 }
