8e3ba3ac975755c5b9ae5acd4e8391fbda40ea70
Michael Sydney Moore <michaelsydneymoore@hotmail.com>
2020-04-18 00:52:27 +0100

    Cleaned up functions

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Axios from "axios";
 import "./App.css";
 
 function App() {
-  const [descrip, setDescrip] = useState([]);
+  const [display, setdisplay] = useState([]);
+
   useEffect(() => {
     async function getData() {
       const response = await Axios.get(
@@ -13,35 +14,44 @@ function App() {
       const repoName = response.data.map((item) => item.name);
       const repoDescription = response.data.map((item) => item.description);
       const githubLink = response.data.map((item) => item.html_url);
-      const updatedAt = response.data.map((item) => item.updated_at);
-      console.log(updatedAt);
-      const displayArray = repoName.reduce(
-        (acc, i, ind) =>
-          acc.push(i, repoDescription[ind], githubLink[ind]) && acc,
-        []
-      );
-      setDescrip([...displayArray]);
+
+      const displayArray = concatArray(repoName, repoDescription, githubLink);
+
+      setdisplay([...displayArray]);
     }
+
     getData();
   }, []);
 
   // Wrapped in useEffect to stop it re rendering
 
   return (
-    <div className="flex">
-      {descrip.map((item, index) => {
-        if (item === null) {
-          return <div className="description">{"no description"}</div>;
-        }
-        if (index % 3 === 0) {
-          return <div className="title">{item}</div>;
-        }
-        if ((index + 1) % 3 === 0) {
-          return <a href={item}>{"Github Link"}</a>;
-        } else return <div className="description">{item}</div>;
-      })}
-    </div>
+    <>
+      <div className="flex">
+        {display.map((item, index) => {
+          if (item === null) {
+            return <div className="description">{"no description"}</div>;
+          }
+          if (index % 3 === 0) {
+            return <div className="title">{item}</div>;
+          }
+          if ((index + 1) % 3 === 0) {
+            return <a href={item}>{"Github Link"}</a>;
+          } else return <div className="description">{item}</div>;
+        })}
+      </div>
+    </>
   );
+
+  function concatArray(repoName, repoDescription, githubLink) {
+    const displayArray = [];
+    repoName.forEach((item, index) => {
+      displayArray.push(item);
+      displayArray.push(repoDescription[index]);
+      displayArray.push(githubLink[index]);
+    });
+    return displayArray;
+  }
 }
 
 export default App;
