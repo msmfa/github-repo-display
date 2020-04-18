1f67f55f415cde0acf4d6043752f89fa697cb7c6
Michael Sydney Moore <michaelsydneymoore@hotmail.com>
2020-04-18 16:26:14 +0100

    Added = to to fix bu

--- src/components/timeSince.js
+++ src/components/timeSince.js
@@ -5,19 +5,19 @@ export function timeSince(date) {
     return interval + " years ago";
   }
   interval = Math.floor(seconds / 2592000);
-  if (interval > 1) {
+  if (interval >= 1) {
     return interval + " months ago";
   }
   interval = Math.floor(seconds / 86400);
-  if (interval > 1) {
+  if (interval >= 1) {
     return interval + " days ago";
   }
   interval = Math.floor(seconds / 3600);
-  if (interval > 1) {
+  if (interval >= 1) {
     return interval + " hours ago";
   }
   interval = Math.floor(seconds / 60);
-  if (interval > 1) {
+  if (interval >= 1) {
     return interval + " minutes ago";
   }
   return Math.floor(seconds) + " seconds ago";
